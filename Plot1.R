2c0230c2faf0e21b9179f7b744e314d59873f387
me2Siva <siva.c@cox.net>
2017-07-24 01:09:49 -0600

    Add files via upload
    Week#1 Assignment

---
+++ Plot1.R
@@ -0,0 +1,21 @@
+## Loading the data
+EPCdata <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+EPCdata$Date <- as.Date(EPCdata$Date, format="%d/%m/%Y")
+
+data <- subset(EPCdata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(EPCdata)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Hist graph : Global Active Power(Kilowatts) vs. Frequency  
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+## closedevice
+dev.off()
